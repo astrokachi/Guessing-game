fbf918a5bf9d1be810731c5da274e536c3f060dd
Kachi <astrokachi@gmail.com>
2023-02-12 22:43:16 +0100

    feat: added error display fn, rf button display

--- public/logic.js
+++ public/logic.js
@@ -1,17 +1,18 @@
+import { convertStrings } from "./utils.js";
 let socket;
 const btn1 = document.querySelector("#btn1");
 const stage1 = document.querySelector("#stage1");
 const stage2 = document.querySelector("#stage2");
-const btn2 = document.querySelector("#btn2");
 const actionBtncon = document.querySelector("#actionBtncon");
 let newBtn = document.createElement("button");
-import { convertStrings } from "./utils.js";
-
-// let newEl = document.createElement("div");
+let newEl = document.createElement("div");
+let createBtnEl;
 
 const btnClass = convertStrings(
 	"middle none center block mx-auto rounded-lg bg-purple-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-purple-500/20 transition-all hover:shadow-lg hover:shadow-purple-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
 );
+
+const elClass = convertStrings("text-red-600 font-sm");
 
 btn1.addEventListener("click", function () {
 	const inputName = document.querySelector("#name").value;
@@ -22,20 +23,45 @@ btn1.addEventListener("click", function () {
 		stage2.classList.remove("hidden");
 
 		socket.on("role", (users) => {
-			const user = users.filter((user) => user.id === socket.id)[0];
-			console.log(user);
-			// update users
-			playerType.innerHTML = `You are a ${user.role}`;
-			changeNo.innerHTML = `players: ${users.length}`;
+			if (!users.gameMaster) {
+				//get user
+				const user = users.filter((user) => user.id === socket.id)[0];
+				console.log(user);
+				// display info
+				playerType.innerHTML = `You are a ${user.role}, ${user.name}`;
+				changeNo.innerHTML = `players: ${users.length}`;
+				//disply join button
+				createBtn("join");
+			}
 
-			//display btn create or join depending on role of user
-
-			if (user.role === "game master" && actionBtncon.childElementCount === 0) {
-				newBtn.id = "Create";
-				newBtn.innerHTML = "Create game";
-				newBtn.classList.add(...btnClass);
-				actionBtncon.appendChild(newBtn);
+			if (users.gameMaster) {
+				let gm = users.gameMaster;
+				//display info
+				playerType.innerHTML = `You are the ${gm.role}, ${gm.name}`;
+				changeNo.innerHTML = `players: ${users.players.length}`;
+				//display btn create button
+				createBtn("create");
+				createBtnEl = document.querySelector("#create");
+				createBtnEl.addEventListener("click", () => {
+					socket.emit("start");
+				});
+			}
+		});
+		socket.on("error", (message) => {
+			//display error message
+			newEl.innerHTML = message;
+			newEl.classList.add(elClass);
+			if (actionBtncon.childElementCount === 1) {
+				actionBtncon.appendChild(newEl);
 			}
 		});
 	}
 });
+
+function createBtn(name) {
+	newBtn.id = name;
+	newBtn.innerHTML = `${name} game`;
+	newBtn.classList.add(...btnClass);
+	actionBtncon.inner = "";
+	actionBtncon.appendChild(newBtn);
+}
