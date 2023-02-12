b7e550e662e8da69dd131956d7cbde8a0966748f
Kachi <astrokachi@gmail.com>
2023-02-12 04:58:42 +0100

    feat: added static path and created player logic

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require('path')
 //set up express
 const express = require("express");
 const app = express();
@@ -8,13 +9,18 @@ const server = http.createServer(app);
 //initialize socket.io
 const io = require("socket.io")(server, { cors: { origin: "*" } });
 
+const players = []
+
 const PORT = process.env.PORT;
 
 app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get("/", (req, res) => {
 	res.render("index");
 });
+
 
 //start server
 server.listen(PORT, () => {
@@ -24,4 +30,20 @@ server.listen(PORT, () => {
 //socket logic
 io.on("connection", (socket) => {
 	console.log("Someone connected!");
+
+    socket.on("name", (data) => {
+        console.log(data)
+        if(players.length === 0){
+            const player = {
+                name: data,
+                role: admin,
+
+            }
+        }else {
+            const player = {
+                name: data,
+                role: player
+            }
+        }
+    })
 });
