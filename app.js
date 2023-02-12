71729cc70b8360a2fded538c793cb4a438706933
Kachi <astrokachi@gmail.com>
2023-02-12 22:38:49 +0100

    feat: handled disconnect event and rf role event

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ const io = require("socket.io")(server, { cors: { origin: "*" } });
 
 const PORT = process.env.PORT;
 
+let gameMaster = null;
 let players = [];
+let gameSession = false;
 
 app.set("view engine", "ejs");
 
@@ -38,15 +40,13 @@ io.on("connection", (socket) => {
 
 	socket.on("name", (data) => {
 		console.log(data);
-		if (players.length === 0) {
-			const player = {
+		if (!gameMaster) {
+			gameMaster = {
 				name: data,
 				role: "game master",
 				id: socket.id,
 			};
-			players.push(player);
-			socket.emit("role", players);
-			socket.broadcast.emit("role", players);
+			socket.emit("role", { gameMaster, players: players });
 		} else {
 			const player = {
 				name: data,
@@ -56,13 +56,37 @@ io.on("connection", (socket) => {
 			};
 			players.push(player);
 			socket.emit("role", players);
+			socket.broadcast.emit("role", { gameMaster, players: players });
 			socket.broadcast.emit("role", players);
 		}
 	});
 
 	socket.on("disconnect", () => {
-		players = [...players.filter((player) => player.id !== socket.id)];
+		if (gameMaster?.id === socket.id) {
+			if (players.length > 0) {
+				const newGm = players.splice(0, 1)[0];
+				gameMaster = {
+					name: newGm.name,
+					role: "game master",
+					id: newGm.id,
+				};
+				console.log(players, gameMaster);
+			} else {
+				gameMaster = null;
+			}
+		} else {
+			players = [...players.filter((player) => player.id !== socket.id)];
+		}
+		socket.broadcast.emit("role", { gameMaster, players: players });
 		socket.broadcast.emit("role", players);
-		console.log(players);
+	});
+
+	socket.on("start", () => {
+		if (players.length < 3 || !gameMaster) {
+			socket.emit(
+				"error",
+				"There must be at least 3 players and a game master"
+			);
+		}
 	});
 });
