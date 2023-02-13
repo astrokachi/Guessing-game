62a408f6ebc566c5bdc8547a537aa5bb05079fc3
Kachi <astrokachi@gmail.com>
2023-02-13 01:11:58 +0100

    chore: added function to create buttons

--- public/utils.js
+++ public/utils.js
@@ -1,8 +1,19 @@
-function createQuestion() {}
-
 function convertStrings(str) {
 	str = str.split(" ");
 	return str;
 }
 
-export { convertStrings };
+function createBtn(btnName, className, btn) {
+	btn.id = btnName;
+	btn.innerHTML = `${btnName} game`;
+	btn.classList.add(...className);
+	actionBtncon.inner = "";
+	actionBtncon.appendChild(btn);
+}
+
+function updateInfo(user, users) {
+	playerType.innerHTML = `You are a ${user.role}, ${user.name}`;
+	changeNo.innerHTML = `players: ${users.length}`;
+}
+
+export { convertStrings, createBtn, updateInfo };
